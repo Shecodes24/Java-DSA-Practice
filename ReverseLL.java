1a2f03f60cce28e99c9d1d37441b02ccff915bcc
Shivani S <116503259+Shecodes24@users.noreply.github.com>
2025-06-04 20:08:19 +0530

    Reverse a Singly Linked List

--- ReverseLL.java
+++ ReverseLL.java
@@ -17,13 +17,9 @@ public class LinkedList
     public void addFirst(int data)
     {   
         Node newNode = new Node(data);
-        if(head == null)
-        {
-            head = newNode;
-            return;
-        }
         newNode.next =  head;
         head = newNode;
+        return;
     }
 
     public void display()
