2fa5f9a571efb32fa38e891d5b344c7b0216c6f0
Shivani S <116503259+Shecodes24@users.noreply.github.com>
2025-06-04 16:07:09 +0530

    Create ReverseLL.java

---
+++ ReverseLL.java
@@ -0,0 +1,65 @@
+public class ReverseLL
+{
+    public static class Node
+    {
+        int data;
+        Node next;
+
+        Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+
+    public void addFirst(int data)
+    {   
+        Node newNode = new Node(data);
+        if(head == null)
+        {
+            head = newNode;
+            return;
+        }
+        newNode.next =  head;
+        head = newNode;
+    }
+
+    public Node reverse()
+    {
+        Node prev = null;
+        Node curr = head;
+        Node next = null;
+
+        while(curr != null)
+        {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args)
+    {
+        ReverseLL ll = new ReverseLL();
+        ll.addFirst(50);
+        ll.addFirst(40);
+        ll.addFirst(30);
+        ll.addFirst(20);
+        ll.addFirst(10);
+        System.out.print("Before Reversal: ");
+        ll.display();
+        head = ll.reverse();
+        System.out.print("\nAfter Reversal: ");
+        ll.display();
+    }
+}
+
+/*
+OUTPUT:
+Before Reversal: 10--->20--->30--->40--->50--->null
+After Reversal: 50--->40--->30--->20--->10--->null
+*/
